b7d825f85522b78b79b90fe9512720111dbe5222
Breck Yunits <breck7@gmail.com>
2017-10-26 06:45:57 -0700

    Added logging of run files to assist with refactors

--- consoleApp.js
+++ consoleApp.js
@@ -1,5 +1,6 @@
 const TreeProgram = require("./index.js")
 const fs = require("fs")
+const os = require("os")
 
 class ConsoleApp {
   constructor(languagesObj = {}) {
@@ -18,6 +19,7 @@ class ConsoleApp {
       "run [programPath] - Execute a Tree Language Program",
       "create [languageName] - Create a new Tree Language",
       "list - List installed Tree Languages",
+      "programs - List all Tree Programs ever parsed with this cli tool",
       "version - List installed Tree Notation version"
     ]
     commands.sort()
@@ -41,6 +43,7 @@ class ConsoleApp {
   }
 
   check(programPath) {
+    this._logProgramPath(programPath)
     const languagePath = this._getLanguagePathOrThrow(programPath)
     const program = TreeProgram.makeProgram(programPath, languagePath)
     const errors = program.getProgramErrors()
@@ -56,6 +59,7 @@ class ConsoleApp {
   }
 
   compile(programPath) {
+    this._logProgramPath(programPath)
     // todo: allow user to provide destination
     const languagePath = this._getLanguagePathOrThrow(programPath)
     const program = TreeProgram.makeProgram(programPath, languagePath)
@@ -64,7 +68,19 @@ class ConsoleApp {
     fs.writeFileSync(path, compiledCode, "utf8")
   }
 
+  _getLogFilePath() {
+    return os.homedir() + "/programs.tree"
+  }
+
+  programs() {
+    const data = fs.readFileSync(this._getLogFilePath(), "utf8")
+    console.log(new TreeProgram(new TreeProgram(data.trim()).toObject()).getKeywords().join("\n"))
+  }
+
   _logProgramPath(programPath) {
+    const logFilePath = this._getLogFilePath()
+    const line = `${programPath} ${Date.now()}\n`
+    fs.appendFile(logFilePath, line, "utf8", () => {})
     // everytime you run/check/compile a tree program, log it by default.
     // that way, if a language changes or you need to do refactors, you have the
     // data of file paths handy..

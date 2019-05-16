2dc1350d42d4e01f3a13dcd1d4d9be6278b622bc
Breck Yunits <breck7@gmail.com>
2019-05-16 11:17:15 -1000

    Grammars cleanup

---
+++ langs/numbers/numbers.test.js
@@ -0,0 +1,21 @@
+#! /usr/local/bin/node --use_strict
+
+const NumbersProgram = require("./NumbersProgram.js")
+
+const testTree = {}
+
+testTree.all = equal => {
+  // Arrange
+  const program = new NumbersProgram(`+ 2 2 1 2
+- 2 2`)
+
+  // Act/Assert
+  equal(program.getProgramErrors().length, 0)
+  equal(program.executeSync().join(" "), `7 0`)
+
+  // A/A/A
+  equal(new NumbersProgram(`+ 2 2 1 1`).executeSync().join(""), `6`)
+}
+
+/*NODE_JS_ONLY*/ if (!module.parent) require("../../tests/testTreeRunner.js")(testTree)
+module.exports = testTree

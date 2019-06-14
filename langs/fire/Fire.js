4719a14acc9f59b54b91e5407363902de91c7175
Breck Yunits <breck7@gmail.com>
2019-06-13 15:00:51 -1000

    quick commit

--- langs/fire/Fire.js
+++
@@ -1,7 +0,0 @@
-const jtree = require("../../index.js")
-
-module.exports = {
-  Program: jtree.getProgramConstructor(__dirname + "/fire.grammar"),
-  Constants: require("./FireLang.js").FireConstants,
-  Nodes: require("./FireLang.js")
-}
--- langs/fire/fire.test.js
+++ langs/fire/fire.test.js
@@ -1,12 +1,12 @@
 #! /usr/local/bin/node --use_strict
 
-const Fire = require("./Fire.js")
+const FireProgram = require("./Fire.js")
 
 const testTree = {}
 
 testTree.all = equal => {
   // Arrange
-  const program = new Fire.Program(`add ten 2 3 5`)
+  const program = new FireProgram(`add ten 2 3 5`)
 
   // Act/Assert
   equal(program.getProgramErrors().length, 0)

8663395cfaf82562df7ad18cfd29f2e984ed20b1
Breck Yunits <breck7@gmail.com>
2019-07-15 10:45:33 -1000

    Travis fix

--- tasks/test.js
+++ tasks/test.js
@@ -2,7 +2,7 @@
 const reporter = require("tap-mocha-reporter")
 const exec = require("child_process").exec
 
-const proc = exec(__dirname + "/testAll.js")
+const proc = exec("node " + __dirname + "/testAll.js")
 
 proc.stdout.pipe(reporter("dot"))
 proc.stderr.on("data", data => console.error("stderr: " + data.toString()))

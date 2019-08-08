16b96f310170cf3b0ca5f530230a0ae1251c71dd
Breck Yunits <breck7@gmail.com>
2019-08-08 00:37:46 -0400

    Added products

---
+++ products/SandboxServer.node.js
@@ -0,0 +1,135 @@
+"use strict";
+//tooling product jtree.node.js
+//tooling product jtree.browser.js
+//tooling product cli.node.js
+//tooling product treeBase.node.js
+//tooling product SandboxServer.node.js
+Object.defineProperty(exports, "__esModule", { value: true });
+//tooling product SandboxServer.node.js
+const jtree = require("../products/jtree.node.js");
+class TypeScriptRewriter {
+    constructor(fileStr) {
+        this._str = fileStr;
+    }
+    addUseStrict() {
+        this._str = `"use strict";\n` + this._str;
+        return this;
+    }
+    removeRequires() {
+        this._str = this._str.replace(/(\n|^)const .* \= require\(.*/g, "$1");
+        return this;
+    }
+    _removeAllLinesStartingWith(prefix) {
+        this._str = this._str
+            .split("\n")
+            .filter(line => !line.startsWith(prefix))
+            .join("\n");
+        return this;
+    }
+    removeNodeJsOnlyLines() {
+        return this._removeAllLinesStartingWith("/*NODE_JS_ONLY*/");
+    }
+    removeHashBang() {
+        this._str = this._str.replace(/^\#\![^\n]+\n/, "");
+        return this;
+    }
+    removeUseStrict() {
+        const strict = `"use strict";\n`;
+        this._str = this._str.replace(strict, "");
+        return this;
+    }
+    addUseStrictIfNotPresent() {
+        const str = `"use strict"`;
+        this._str = this._str.startsWith(str) ? this._str : str + ";\n" + this._str;
+        return this;
+    }
+    removeNodeJsOnly() {
+        this._str = this._str.replace(/\/\*NODE_JS_ONLY\*\/[^\n]+\n/g, "\n");
+        return this;
+    }
+    removeImports() {
+        // todo: what if this spans multiple lines?
+        this._str = this._str.replace(/(\n|^)import .* from .*/g, "$1");
+        this._str = this._str.replace(/(\n|^)\/\*FOR_TYPES_ONLY\*\/ import .* from .*/g, "$1");
+        this._str = this._str.replace(/(\n|^)import {[^\}]+} ?from ?"[^\"]+"/g, "$1");
+        return this;
+    }
+    removeExports() {
+        this._str = this._str.replace(/(\n|^)export default .*/g, "$1");
+        this._str = this._str.replace(/(\n|^)export {[^\}]+}/g, "$1");
+        return this;
+    }
+    changeDefaultExportsToWindowExports() {
+        this._str = this._str.replace(/\nexport default ([^;]*)/g, "\nwindow.$1 = $1");
+        // todo: should we just switch to some bundler?
+        const matches = this._str.match(/\nexport { [^\}]+ }/g);
+        if (matches)
+            this._str = this._str.replace(/\nexport { ([^\}]+) }/g, matches[0]
+                .replace("export {", "")
+                .replace("}", "")
+                .trim()
+                .split(/ /g)
+                .map(name => name.replace(",", "").trim())
+                .map(mod => `\nwindow.${mod} = ${mod}`)
+                .join("\n"));
+        return this;
+    }
+    static treeToJs(filepath, file) {
+        const TreeUtils = jtree.Utils;
+        const filename = TreeUtils.getFileName(filepath);
+        const baseName = TreeUtils.removeFileExtension(filename);
+        const extension = TreeUtils.getFileExtension(filename);
+        const varName = baseName + extension.charAt(0).toUpperCase() + extension.substr(1);
+        const basePath = TreeUtils.getPathWithoutFileName(filepath);
+        // const newPath = basePath + "/" + varName + ".compiled.js"
+        const lines = file.split(/\n/);
+        const jsCode = `"use strict";
+window.${varName} = \`${TreeUtils.escapeBackTicks(lines.join("\n"))}\``;
+        return jsCode;
+    }
+    changeNodeExportsToWindowExports() {
+        // todo: should we just switch to some bundler?
+        const reg = /\nmodule\.exports = { ?([^\}]+) ?}/;
+        const matches = this._str.match(reg);
+        if (matches) {
+            this._str = this._str.replace(reg, matches[1]
+                .split(/,/g)
+                .map(mod => mod.replace(/\s/g, ""))
+                .map(mod => `\nwindow.${mod} = ${mod}`)
+                .join("\n"));
+        }
+        this._str = this._str.replace(/\nmodule\.exports \= ([^;^{]*)/g, "\nwindow.$1 = $1");
+        this._str = this._str.replace(/\nexports\.default \= ([^;]*)/g, "\nwindow.$1 = $1");
+        return this;
+    }
+    getString() {
+        return this._str;
+    }
+}
+//tooling product SandboxServer.node.js
+const express = require("express");
+const { readFile } = require("fs");
+class SandboxServer {
+    start(port) {
+        const app = express();
+        app.get("/*.js", (req, res) => {
+            const filename = req.path.substr(1);
+            readFile(__dirname + "/../" + filename, "utf8", (err, code) => {
+                if (err)
+                    throw err;
+                res.send(new TypeScriptRewriter(code)
+                    .removeRequires()
+                    .removeHashBang()
+                    .removeNodeJsOnlyLines()
+                    .changeNodeExportsToWindowExports()
+                    .getString());
+            });
+        });
+        app.use(express.static(__dirname + "/../"));
+        app.listen(port, () => {
+            console.log(`Running sandbox. cmd+dblclick: http://localhost:${port}/sandbox`);
+        });
+    }
+}
+
+module.exports = {SandboxServer}

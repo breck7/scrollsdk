01dd2c379b7d21c404fe0af68272c26b177f72f3
Breck Yunits <breck7@gmail.com>
2020-12-20 14:11:26 -1000

    New: add share link to sandbox. Add toJson and toGridJson consoles

--- sandbox/SandboxApp.ts
+++ sandbox/SandboxApp.ts
@@ -5,10 +5,14 @@ const { jtree } = require("../index.js")
 
 declare var jQuery: any
 
+
+// Todo: add inputs at the top to change the edge, node, and cell delimiters.
+
 class SandboxApp extends AbstractTreeComponent {
   createParser() {
     return new jtree.TreeNode.Parser(undefined, {
       tableComponent: tableComponent,
+      shareComponent: shareComponent,
       githubTriangleComponent: githubTriangleComponent,
       headerComponent: headerComponent,
       TreeComponentFrameworkDebuggerComponent: TreeComponentFrameworkDebuggerComponent
@@ -16,7 +20,7 @@ class SandboxApp extends AbstractTreeComponent {
   }
 
   loadJsonSampleCommand() {
-    jQuery("#jsonConsole")
+    jQuery("#toJsonSubset")
       .val(
         `{
  "name": "jtree",
@@ -33,43 +37,81 @@ class SandboxApp extends AbstractTreeComponent {
       .keyup()
   }
 
+  private get _shareLink() {
+    return jQuery("#shareLink")
+  }
+
+  private _updateShareLink() {
+    const url = new URL(location.href)
+    url.hash = ""
+    const base = url.toString()
+    this._shareLink.val(base + this.toShareLink())
+  }
+
+  toShareLink() {
+  	const treeCode = localStorage.getItem("tree")
+  	if (!treeCode)
+  		return ""
+    const tree = new jtree.TreeNode()
+    tree.appendLineAndChildren("tree", treeCode)
+    return "#" + encodeURIComponent(tree.toString())
+  }
+
+  private treeFromDeepLink() {
+    const hash = location.hash
+    if (hash.length < 2) return ""
+
+    return new jtree.TreeNode(decodeURIComponent(hash.substr(1))).getNode("tree")
+  }
+
   async treeComponentDidMount() {
+  	const app = this
     // todo: refactor!!! split these into components
     jQuery(document).ready(function() {
       const treeConsole = jQuery("#treeConsole")
-      const jsonConsole = jQuery("#jsonConsole")
+      const toJsonSubset = jQuery("#toJsonSubset")
       const outlineConsole = jQuery("#outlineConsole")
       const csvConsole = jQuery("#csvConsole")
       const xmlConsole = jQuery("#xmlConsole")
       const htmlConsole = jQuery("#htmlConsole")
       const tableConsole = jQuery("#tableConsole")
       const htmlCubeConsole = jQuery("#htmlCubeConsole")
+      const gridJsonConsole = jQuery("#gridJsonConsole")
+      const jsonConsole = jQuery("#jsonConsole")
       const yamlConsole = jQuery("#yamlConsole")
 
       // Init vars
-      if (localStorage.getItem("tree")) treeConsole.val(localStorage.getItem("tree"))
+      const deepLink = app.treeFromDeepLink()
+      if (deepLink)
+      		treeConsole.val(deepLink.childrenToString())
+      else if (localStorage.getItem("tree")) treeConsole.val(localStorage.getItem("tree"))
 
       const updateAll = (tree: any, eventSource: any) => {
         if (eventSource !== treeConsole) treeConsole.val(tree.toString())
-        if (eventSource !== jsonConsole) jsonConsole.val(tree.toJsonSubset())
+        if (eventSource !== toJsonSubset) toJsonSubset.val(tree.toJsonSubset())
         if (eventSource !== outlineConsole) outlineConsole.html(tree.toOutline())
         if (eventSource !== csvConsole) csvConsole.val(tree.toCsv())
         if (eventSource !== xmlConsole) xmlConsole.val(tree.toXml())
         if (eventSource !== htmlConsole) htmlConsole.html(tree.toHtml())
         if (eventSource !== tableConsole) tableConsole.html(tree.toTable())
+        if (eventSource !== gridJsonConsole) gridJsonConsole.val(tree.toGridJson())
+        if (eventSource !== jsonConsole) jsonConsole.val(tree.toJson())
         if (eventSource !== htmlCubeConsole) htmlCubeConsole.html(tree.toHtmlCube())
         if (eventSource !== yamlConsole) yamlConsole.html(tree.toYaml())
 
         let win = <any>window
         win.tree = tree
         localStorage.setItem("tree", tree.toString())
+        app._updateShareLink() // todo: where to put this?
       }
 
       // Bind listeners
       treeConsole.on("keyup", () => updateAll(new jtree.TreeNode(treeConsole.val()), treeConsole))
-      jsonConsole.on("keyup", () => updateAll(jtree.TreeNode.fromJsonSubset(jsonConsole.val()), jsonConsole))
+      toJsonSubset.on("keyup", () => updateAll(jtree.TreeNode.fromJsonSubset(toJsonSubset.val()), toJsonSubset))
       csvConsole.on("keyup", () => updateAll(jtree.TreeNode.fromCsv(csvConsole.val()), csvConsole))
       xmlConsole.on("keyup", () => updateAll(jtree.TreeNode.fromXml(xmlConsole.val()), xmlConsole))
+      gridJsonConsole.on("keyup", () => updateAll(jtree.TreeNode.fromGridJson(gridJsonConsole.val()), gridJsonConsole))
+      jsonConsole.on("keyup", () => updateAll(jtree.TreeNode.fromJson(jsonConsole.val()), jsonConsole))
 
       // Trigger start
       treeConsole.keyup()
@@ -122,6 +164,8 @@ textarea,pre
  font-size 14px
  white-space pre
  overflow scroll
+input
+ border 1px solid black
 pre
  line-height 14px
 .keyword
@@ -164,6 +208,28 @@ class headerComponent extends AbstractTreeComponent {
   }
 }
 
+class shareComponent extends AbstractTreeComponent {
+  toStumpCode() {
+    return `div
+ id shareDiv
+ span Share
+ input
+  id shareLink
+  readonly`
+  }
+  toHakonCode() {
+    return `#shareDiv
+ font-size 16px
+ width 100%
+ span
+  width 50px
+  display inline-block
+ input
+  font-size 16px
+  padding 5px`
+  }
+}
+
 class githubTriangleComponent extends AbstractGithubTriangleComponent {
   githubLink = `https://github.com/treenotation/jtree/tree/master/sandbox`
 }
@@ -173,16 +239,25 @@ class tableComponent extends AbstractTreeComponent {
     return `table
  tr
   td
-   div Tree
+   div Tree Notation
    textarea
     id treeConsole
+  td
+   div toGridJson()
+   textarea
+    id gridJsonConsole
+ tr
+  td
+   div toJson()
+   textarea
+    id jsonConsole
   td
    div
     span toJsonSubset()
     a sample
      clickCommand loadJsonSampleCommand
    textarea
-    id jsonConsole
+    id toJsonSubset
  tr
   td
    div
@@ -217,6 +292,7 @@ class tableComponent extends AbstractTreeComponent {
  tr
   td
    div toHtmlCube()
+    title Experimental. This is a very specific kind of Tree Language.
    div
     id htmlCubeConsole
     style position:relative;`

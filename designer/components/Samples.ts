e7d62835a94148c562bdd96d284fbee2d7cb92e9
Breck Yunits <breck7@gmail.com>
2021-09-25 16:02:24 -1000

    checkpoint

--- designer/components/Samples.ts
+++ designer/components/Samples.ts
@@ -1,6 +1,9 @@
 const { AbstractTreeComponent } = require("../../products/TreeComponentFramework.node.js")
 const { jtree } = require("../../index.js")
 
+// http://localhost:3333/designer/#url%20https://simoji.pub/simoji.grammar
+// http://localhost:3333/designer/#url%20https://scroll.pub/scrolldown.grammar
+
 class SamplesComponent extends AbstractTreeComponent {
   languages = "newlang hakon stump dumbdown arrow dug iris fire chuck wwt swarm project stamp grammar config jibberish numbers poop".split(" ")
 

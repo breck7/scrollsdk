c8e11e87f178e615162abc05cae11284dabe048b
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2022-07-14 23:05:47 -1000

    Version bump

--- core/TreeNode.ts
+++ core/TreeNode.ts
@@ -2954,7 +2954,7 @@ class TreeNode extends AbstractNode {
     return str ? indent + str.replace(/\n/g, indent) : ""
   }
 
-  static getVersion = () => "53.5.1"
+  static getVersion = () => "53.6.0"
 
   static fromDisk(path: string): TreeNode {
     const format = this._getFileFormat(path)

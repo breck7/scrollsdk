6097e2e099936ab80dd8e57057b71e411ca7ba52
Breck Yunits <breck7@gmail.com>
2022-09-26 05:13:42 -1000

    Bump version. Upgrade scroll version of readme

--- core/TreeNode.ts
+++ core/TreeNode.ts
@@ -2957,7 +2957,7 @@ class TreeNode extends AbstractNode {
     return str ? indent + str.replace(/\n/g, indent) : ""
   }
 
-  static getVersion = () => "56.0.0"
+  static getVersion = () => "56.0.1"
 
   static fromDisk(path: string): TreeNode {
     const format = this._getFileFormat(path)

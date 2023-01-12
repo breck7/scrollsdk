49657bf62c0317bda79e614ac2b764817753874a
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-01-12 04:16:51 -0800

    Remove jtree.node.ts

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { jtreeNode } = require("./products/jtree.node.js")
+const { jtree } = require("./products/jtree.node.js")
 const { Utils } = require("./products/Utils.js")
-const jtree = jtreeNode
 module.exports = { jtree, Utils }

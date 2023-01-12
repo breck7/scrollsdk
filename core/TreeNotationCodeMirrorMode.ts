9dee61ab25178092fcb8ed2281200bcacc1dbda0
Breck Yunits <breck@Brecks-MacBook-Pro.local>
2023-01-12 05:31:50 -0800

    Build standalone CoreMirror file

--- core/TreeNotationCodeMirrorMode.ts
+++ core/TreeNotationCodeMirrorMode.ts
@@ -1,6 +1,5 @@
 import { treeNotationTypes } from "../products/treeNotationTypes"
 
-/*FOR_TYPES_ONLY*/ import { GrammarBackedNode } from "./GrammarLanguage"
 /* Used for Types Only, but we want this line to remain in the combined intermediate TS program */ import * as CodeMirrorLib from "codemirror"
 
 // Adapted from https://github.com/NeekSandhu/codemirror-textmate/blob/master/src/tmToCm.ts
@@ -214,7 +213,7 @@ class TreeNotationCodeMirrorMode {
   private _cmInstance: CodeMirrorLib.EditorFromTextArea
   private _originalValue: string
 
-  _getParsedProgram(): GrammarBackedNode {
+  _getParsedProgram() {
     const source = this._getProgramCodeFn(this._cmInstance) || ""
     if (!this._cachedProgram || this._cachedSource !== source) {
       this._cachedSource = source

ddc16146e9d314aadf4973d0650b3e42d58fc5c9
Breck Yunits <breck7@gmail.com>
2023-03-07 07:53:07 -1000

    Bump

--- products/Utils.browser.js
+++ products/Utils.browser.js
@@ -108,7 +108,7 @@ class Utils {
     return str.charAt(0).toUpperCase() + str.slice(1)
   }
   // generate a random alpha numeric hash:
-  static getRandomCharacters(len) {
+  static getRandomCharacters(length) {
     const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     let result = ""
     for (let i = 0; i < length; i++) {

2199c48a8709cc3d0b91f60a440eafad5f9fb39f
Breck Yunits <breck7@gmail.com>
2025-03-23 08:08:50 -0400

    Perf work

--- particle/Particle.ts
+++ particle/Particle.ts
@@ -3122,7 +3122,7 @@ class Particle extends AbstractParticle {
     return str ? indent + str.replace(/\n/g, indent) : ""
   }
 
-  static getVersion = () => "102.0.0"
+  static getVersion = () => "103.0.0"
 
   static fromDisk(path: string): Particle {
     const format = this._getFileFormat(path)
